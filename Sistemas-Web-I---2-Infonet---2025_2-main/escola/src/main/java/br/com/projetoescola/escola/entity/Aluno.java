ec765959915dc4630db9f78cc60425d070135417
LuAllencar <luana.amenezes06@gmail.com>
2025-09-04 19:22:53 -0300

    Add files via upload
    Projeto inteiro

---
+++ Sistemas-Web-I---2-Infonet---2025_2-main/escola/src/main/java/br/com/projetoescola/escola/entity/Aluno.java
@@ -0,0 +1,40 @@
+package br.com.projetoescola.escola.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aluno{
+
+    //definição dos atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idAluno;
+    
+    @Column(nullable = false, length = 40)
+    private String nomeAluno;
+    
+    @Column(nullable = false, length = 11)
+    private String telefoneAluno;
+    
+    @Column(nullable = false, length = 40)
+    private String enderecoAluno;
+    
+    @Column(nullable = false)
+    private Integer rmAluno;
+    
+    @Column(nullable = false, length = 11)
+    private String cpfAluno;
+
+}
